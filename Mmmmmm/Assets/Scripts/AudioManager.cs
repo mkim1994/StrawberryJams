7b60d8617fbc1ecf26c5b6ffd239b3523b38336f
mkim1994 <mkim2@andrew.cmu.edu>
2017-02-22 19:49:48 -0500

    audiomanager setup

---
+++ Mmmmmm/Assets/Scripts/AudioManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour {
+
+	public AudioSource bgm;
+	public AudioSource UIbuttonclick;
+	public AudioSource UIbuttonback;
+	public AudioSource purchase;
+
+	/*
+	 * Use "GameObject.FindWithTag("AudioManager").GetComponent<AudioManager>()" to access
+	 * this script. Just follow the format and feel free to change this script any time 
+	 * to add additional AudioSources.
+	 * 
+	 * */
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
