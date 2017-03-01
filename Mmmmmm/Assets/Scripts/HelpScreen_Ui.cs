8a35094a568d05a80453a5d1e9286bda55b115e3
Nivetha Kannan <nivetha.kan@gmail.com>
2017-02-28 22:38:09 -0500

    Some Start Screen & Help Screen

---
+++ Mmmmmm/Assets/Scripts/HelpScreen_Ui.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelpScreen_Ui : MonoBehaviour {
+
+	public GameObject help0;
+	public GameObject help1;
+	public GameObject help2;
+
+	// Use this for initialization
+	public void ChangePanelNext()
+	{
+		if (help0.activeSelf) {
+			help0.SetActive (false);
+			help1.SetActive (true);
+		} else if (help1.activeSelf) {
+			help1.SetActive (false);
+			help2.SetActive (true);
+		} 
+
+	}
+
+	public void ChangePanelPrevious(){
+
+
+		if (help1.activeSelf) {
+			help1.SetActive (false);
+			help0.SetActive (true);
+		} else if (help2.activeSelf) {
+			help2.SetActive (false);
+			help1.SetActive (true);
+		}
+	}
+}
